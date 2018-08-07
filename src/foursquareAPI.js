4f64d1e6e51bc079f6524f6add7caf74c2ee0412
Jana <36570271+JanaHor@users.noreply.github.com>
2018-08-07 19:57:26 +0200

    Update foursquareAPI.js
    fixed typo in const url

--- src/foursquareAPI.js
+++ src/foursquareAPI.js
@@ -1,7 +1,7 @@
 export default function FoursquareData(id, infowindow) {
   const clientID = "GB1QF443M30DNP54WJOV24WP0WHXSSHLJKSWLTT2AEYQCEE1"
   const clientSecret = "FJ5GUUOPWHO5LVM2NL2MYBM40OWSDIPPU4FNWMZULMZU042V"
-  const url = "https://api.foursquare.com/v2/venues/${id}?client_id=${clientID}&client_secret=${clientSecret}&v=20180502"
+  const url = `https://api.foursquare.com/v2/venues/${id}?client_id=${clientID}&client_secret=${clientSecret}&v=20180807`
 
   fetch(url)
     .then(function (response) {
